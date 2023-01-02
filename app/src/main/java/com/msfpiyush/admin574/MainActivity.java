63245f6efb65c84a75dba6aa628ce48bed8c11bd
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-02 18:53:23 +0530

    Activity App1.1.2 ( Add Activity option on MainActivity )

--- app/src/main/java/com/msfpiyush/admin574/MainActivity.java
+++ app/src/main/java/com/msfpiyush/admin574/MainActivity.java
@@ -7,9 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-Button b1,b2;
+Button b1,b2,b3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +29,15 @@ Button b1,b2;
             public void onClick(View view) {
                 Intent RadioButton = new Intent(MainActivity.this,CustomRadioButton.class);
                 startActivity(RadioButton);
+            }
+        });
+        b3=findViewById(R.id.b3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent OpenSpinnerDemo = new Intent(MainActivity.this,SpinnerDemo.class);
+                Toast.makeText(MainActivity.this, "Hurray !😁", Toast.LENGTH_SHORT).show();
+                startActivity(OpenSpinnerDemo);
             }
         });
     }
--- app/src/main/java/com/msfpiyush/admin574/SpinnerDemo.java
+++ app/src/main/java/com/msfpiyush/admin574/SpinnerDemo.java
@@ -1,17 +1,21 @@
 package com.msfpiyush.admin574;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.Button;
 
 public class SpinnerDemo extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 String[] City = {" ","Ahemdabad","Surat","Vadodara","Rajkot","Bhavnagar","Jamnagar","Gandhinagar","Junagadh","Gandhidham","Ananad","Navsari","Morbi"
 ,"Nadiad","Surendranagar","Mehsana","Bharuch","Bhuj","Porbandar","Palanpur","Valsad","Gondal","Veraval","Vapi","Patan","Kalol","Godhara","Amreli","Dhasa"
 ,"Dhola","Botad","Jetpur"};
+Button homesb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,16 @@ String[] City = {" ","Ahemdabad","Surat","Vadodara","Rajkot","Bhavnagar","Jamnag
         setContentView(R.layout.spinnerdemo);
         Spinner CityList = findViewById(R.id.CityList);
         CityList.setOnItemSelectedListener(this);
+
+        homesb=findViewById(R.id.homesb);
+        homesb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent MainMenu=new Intent(SpinnerDemo.this,MainActivity.class);
+                Toast.makeText(SpinnerDemo.this, "👌", Toast.LENGTH_SHORT).show();
+                startActivity(MainMenu);
+            }
+        });
 
         ArrayAdapter Connector = new ArrayAdapter(this, android.R.layout.simple_spinner_item, City);
         Connector.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
