66fe5f27efaa3890dd97b7fd84b4fa7b6119e091
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-16 20:15:37 +0530

    Activity Demo v2.0

--- app/src/main/java/com/msfpiyush/admin574/SimpleListViewDemo.java
+++ app/src/main/java/com/msfpiyush/admin574/SimpleListViewDemo.java
@@ -2,7 +2,10 @@ package com.msfpiyush.admin574;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -18,16 +21,22 @@ public class SimpleListViewDemo extends AppCompatActivity {
 ListView SimpleListView;
 String OsBuild[]={"Apple","Microsoft","Android","Ubuntu","Fedora","Kali","ParrotSec","Debian","BlackBarry","Chromium"};
 int OsIcon[]={R.drawable.apple,R.drawable.microsoft,R.drawable.android,R.drawable.ubuntu,R.drawable.fedora,R.drawable.kali_linux,R.drawable.parrot_sec,R.drawable.debian,R.drawable.blackberry,R.drawable.chrome_os};
-
+AlertDialog.Builder adb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simplelistviewdemo);
+
         ActionBar ad=getSupportActionBar();
+        ad.setHomeAsUpIndicator(R.drawable.backbutton32k1);
         ad.setDisplayHomeAsUpEnabled(true);
+
         SimpleListView=findViewById(R.id.SimpleListView);
         CustomAdapterForListView CustomAdapter=new CustomAdapterForListView(getApplicationContext(),OsBuild,OsIcon);
         SimpleListView.setAdapter(CustomAdapter);
+
+//Obj of alert dialogue box
+        adb=new AlertDialog.Builder(this);
         SimpleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -35,6 +44,23 @@ int OsIcon[]={R.drawable.apple,R.drawable.microsoft,R.drawable.android,R.drawabl
 //                SimpleListView.setOnClickListener(new View.OnClickListener() {
 //                    @Override
 //                    public void onClick(View view) {
+                adb.setMessage("You are choosing "+OsBuild[i]+" ! ");
+                adb.setTitle(" Note ");
+                adb.setCancelable(false);
+                adb.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                adb.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                AlertDialog AlertDialogObject=adb.create();
+                AlertDialogObject.show();
                         LayoutInflater inflater = getLayoutInflater();
                         View layout= inflater.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.LinearLayoutRoot));
                         TextView CustomText=layout.findViewById(R.id.CustomText);
