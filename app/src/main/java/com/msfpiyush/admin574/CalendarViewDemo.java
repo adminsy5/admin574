ee67e19b5d79a44b676319f190ecfb9bd4219aeb
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-26 23:30:44 +0530

    Activity Demo v2.2.34 (CalenderView Add Working)

--- app/src/main/java/com/msfpiyush/admin574/CalendarViewDemo.java
+++ app/src/main/java/com/msfpiyush/admin574/CalendarViewDemo.java
@@ -3,6 +3,9 @@ package com.msfpiyush.admin574;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.database.CursorIndexOutOfBoundsException;
+import android.icu.number.Scale;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +14,11 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.joda.time.DateTimeComparator;
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -19,7 +26,6 @@ public class CalendarViewDemo extends AppCompatActivity {
 EditText newDateGet;
 Button btnClick;
 DatePicker DatePickerDemo1;
-String newDate;
 AlertDialog.Builder adb ;
 
     @Override
@@ -71,6 +77,44 @@ AlertDialog.Builder adb ;
         btnClick=findViewById(R.id.btnClick);
         newDateGet=findViewById(R.id.newDateGet);
 
+        newDateGet.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Calendar c= Calendar.getInstance();
+                int year=c.get(Calendar.YEAR);
+                int month=c.get(Calendar.MONTH);
+                int day=c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog=new DatePickerDialog(CalendarViewDemo.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                        newDateGet.setText(d+"/"+m+1+"/"+y);
+                    }
+                },
+                        day,month,year);
+                datePickerDialog.show();
+                return true;
+            }
+        });
+//        newDateGet.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar c= Calendar.getInstance();
+//                int year=c.get(Calendar.YEAR);
+//                int month=c.get(Calendar.MONTH);
+//                int day=c.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog datePickerDialog=new DatePickerDialog(CalendarViewDemo.this, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+//                        newDateGet.setText(d+"/"+m+1+"/"+y);
+//                    }
+//                },
+//                        year,month,day);
+//                datePickerDialog.show();
+//            }
+//        });
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             DatePickerDemo1.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
                 @Override
@@ -80,13 +124,51 @@ AlertDialog.Builder adb ;
                     sb.append(str)
                             .append(day+"/"+month+1+"/"+year);
                     Toast.makeText(CalendarViewDemo.this, sb.toString(), Toast.LENGTH_SHORT).show();
+//                    newDateGet.setText(day+"/"+month+"/"+year);
                 }
             });
         }
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ShowCurrentDate();
+//                ShowCurrentDate();
+                Calendar c=Calendar.getInstance();
+                String CurrentDate=String.valueOf(c.get(Calendar.DAY_OF_MONTH)+"/"
+                        + c.get(Calendar.MONTH)+1+"/"
+                        + c.get(Calendar.YEAR));
+
+                String NewDate=String.valueOf(c.get(Calendar.DAY_OF_MONTH)-1+"/"
+                        + c.get(Calendar.MONTH)+1+"/"
+                        + c.get(Calendar.YEAR));
+//
+//                DateTimeComparator dateTimeComparator=DateTimeComparator.getDateOnlyInstance();
+//
+//                Date date1=new Date();
+//                Date date2=new Date();
+//
+//                int retVal=dateTimeComparator.compare(date1,date2);
+//                if(retVal==0)
+//                {
+//                    Toast.makeText(CalendarViewDemo.this, "equal", Toast.LENGTH_SHORT).show();
+//                }
+//                else if (retVal>0)
+//                {
+//                    Toast.makeText(CalendarViewDemo.this, "date1", Toast.LENGTH_SHORT).show();
+//                }else
+//                {
+//                    Toast.makeText(CalendarViewDemo.this, "date2", Toast.LENGTH_SHORT).show();
+//                }
+                newDateGet.setText(NewDate);
+
+                if(CurrentDate.compareTo(NewDate) > 1) {
+                    Toast.makeText(CalendarViewDemo.this, "New date", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(CalendarViewDemo.this, "Old date", Toast.LENGTH_SHORT).show();
+                }
+//                Toast.makeText(CalendarViewDemo.this, date, Toast.LENGTH_SHORT).show();
+
             }
         });
     }
